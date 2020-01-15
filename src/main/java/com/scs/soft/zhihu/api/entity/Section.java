571f407299110fb78d307acbc8ea721d4cefce8d
RRapids <tengfeiqaq@gmail.com>
2020-01-15 20:48:34 +0800

    知乎后端

---
+++ src/main/java/com/scs/soft/zhihu/api/entity/Section.java
@@ -0,0 +1,12 @@
+package com.scs.soft.zhihu.api.entity;
+
+/**
+ * @author tf_yuan
+ * @description TODO
+ * @date 2020/1/15
+ */
+public class Section {
+    private String sectionId;
+    private String specialId;
+    private String sectionTitle;
+}
---
+++ src/main/java/com/scs/soft/zhihu/api/service/SpecialService.java
@@ -0,0 +1,17 @@
+package com.scs.soft.zhihu.api.service;
+
+import com.scs.soft.zhihu.api.entity.Special;
+
+import java.util.List;
+import java.util.Map;
+
+public interface SpecialService {
+    /**
+     * 查询所有专题
+     */
+    List<Map> selectAll();
+    /**
+     * 查询最新专题
+     */
+    List<Special> selectRecent();
+}
